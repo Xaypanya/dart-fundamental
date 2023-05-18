3db50995d140d273486a24f7ffceb8065cd9a69c
Xaypanya Phongsa <dackogan@gmail.com>
2023-05-18 16:17:47 +0700

    first commit

---
+++ bin/dart_fundamental.dart
@@ -0,0 +1,23 @@
+
+
+void main(List<String> arguments) {
+  String myString = 'Hello world!';
+  print('hello $myString');
+  print(myString.contains("Hello"));
+
+
+  int myInteger = 5;
+  print(myInteger.toString());
+  print(myInteger);
+  print(myInteger.isOdd);
+  double myDouble = 5.5;
+
+  num myNum = 5.5;
+
+  bool myBool = false;
+
+  dynamic mySomething = 5;
+  mySomething = "hello";
+  mySomething = true;
+  print(mySomething);
+}
